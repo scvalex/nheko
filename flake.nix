3a5cd054783ae883ac11bc82b289064063af9ab4
Alexandru Scvorțov <code@scvalex.net>
2025-04-23 11:12:26 +0100

    build: use fish in nix dev

--- flake.nix
+++ flake.nix
@@ -58,8 +58,8 @@
         # set the environment variables that Qt apps expect
         shellHook = ''
           bashdir=$(mktemp -d)
-          makeWrapper "$(type -p bash)" "$bashdir/bash" "''${qtWrapperArgs[@]}"
-          exec "$bashdir/bash"
+          makeWrapper "$(type -p fish)" "$bashdir/fish" "''${qtWrapperArgs[@]}"
+          exec "$bashdir/fish"
         '';
       };
     };
